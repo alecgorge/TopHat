c84cef298fa1f997954ad21558e25bbcdd599de5
Alec Gorge <alecgorge@gmail.com>
2010-05-23 18:20:26 -0400

    made naming scheme and formatting more consistent.

--- cc-admin/design/login.tpl.php
+++ cc-admin/design/login.tpl.php
@@ -8,13 +8,12 @@
 	<body>
 		<h2><?php _e('login_cc'); ?></h2>
 		<?php
-		$form = new Form('self', 'post', 'test');
-		$form->startFieldset(__('Login'));
-			$form->addInput(__("Username"), 'text', 'uname');
-			$form->addInput(__("Password"), 'password', 'passwd');
-			$form->addSubmit('', 'login', __('login_cc'));
-		$form->endFieldset();
-
+		$form = new Form('self', 'post', 'cc_form_login');
+			$form->startFieldset(__('Login'));
+				$form->addInput(__("Username"), 'text', 'cc_login_uname');
+				$form->addInput(__("Password"), 'password', 'cc_login_passwd');
+				$form->addSubmit('', 'cc_login_login', __('login_cc'));
+			$form->endFieldset();
 		echo $form->endAndGetHTML();
 
 		i18n::restore();
