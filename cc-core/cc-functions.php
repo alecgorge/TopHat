44438e25c419f766fa5dc45d5c636077ff820213
Alec Gorge <alecgorge@gmail.com>
2010-05-23 18:37:35 -0400

    added check_post() which allows for unlimited arguments, all are check to make sure they exist within  as keys. An array of keys to be checked can also be used.

--- cc-core/cc-functions.php
+++ cc-core/cc-functions.php
@@ -19,3 +19,24 @@ function get_css ($what_css) {
 		return CC_PUB_ROOT.CC_CONTENT.'css/'.$what_css.'.css';
 	}
 }
+
+/**
+ * Checks $_POST for each of the arguments passed, or optionally the first argument can be an array of keys to check. Uses array_key_exists.
+ *
+ * @param string $first What to check for in $_POST. Optionally, can be an array of keys to check.
+ * @return boolean If all are present, return true.
+ */
+function check_post ($first) {
+	if(is_array($first)) {
+		return array_key_exists($first, $_POST);
+	}
+	else {
+		$args = func_get_args();
+		foreach($args as $val) {
+			if(!array_key_exists($val, $_POST)) {
+				return false;
+			}
+		}
+		return true;
+   	}
+}
