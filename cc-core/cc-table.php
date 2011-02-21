b3b4943b20a9b8396e62d908ff68df513239dd98
Alec Gorge <alecgorge@gmail.com>
2011-02-21 16:20:54 -0500

    tables can now be built in any order (ie, header after rows, or footer before header), but the html is in the correct order

--- cc-core/cc-table.php
+++ cc-core/cc-table.php
@@ -1,19 +1,30 @@
 <?php
 
 class Table {
-	private $html = '';
+	private $start = '';
+	private $end = '</table>';
+	private $header = '';
+	private $footer = '';
+	private $rows = '';
 	private $rowClasses = array();
-	private $rowCount = 1;
+	private $rowCount = 0;
 
+	/**
+	 * @param string $id The HTML ID of the table.
+	 * @param array $rowClasses An array of strings where each string is a class to be applied the corresponding cell. For example: <code>$rowClasses[0] = "test";</code> would make the first cell in every column have a class of "test".
+	 * @param array $attr An associative array of attributes in the form <code>"attribute name" => "attribute value"</code>. The value is automatically escapped.
+	 * @param int $cellpadding Padding in each cell.
+	 * @param int $cellspacing "Margin" around each cell.
+	 */
 	public function  __construct ($id = '', $rowClasses = array(), $attr = array(), $cellpadding = 0, $cellspacing = 0) {
 		$this->rowClasses = $rowClasses;
-		$this->html = sprintf('<table id="%s" cellpadding="%s" cellspacing="%s" %s>'."\n", $id, $cellpadding, $cellspacing, $this->attr($attr));
+		$this->start = sprintf('<table id="%s" cellpadding="%s" cellspacing="%s" %s>'."\n", $id, $cellpadding, $cellspacing, $this->attr($attr));
 	}
 
 	public function attr ($attr) {
 		$r = "";
 		foreach($attr as $key => $val) {
-			$r .= $key.'="'.$value.'" ';
+			$r .= $key.'="'.htmlspecialchars($val, ENT_COMPAT, 'UTF-8', false).'" ';
 		}
 		return rtrim($r);
 	}
@@ -23,14 +34,20 @@ class Table {
 			$classes = $this->rowClasses;
 		}
 
-		$this->html .= sprintf("\t<tr class=\"%s\">\n", ($this->rowCount % 2 == 0 ? 'even' : 'odd').trim(" ".$rowClass));
-		foreach($values as $k => $v) {
-			$this->html .= sprintf("\t\t<td class='%s'%s>%s</td>\n", $classes[$k], ($colspan ? " colspan='$colspan'" : ""), $v);
-		}
-		$this->html .= "\t</tr>\n";
-
 		$this->rowCount++;
+
+		$this->rows .= sprintf("\t<tr class=\"%s\">\n", ($this->rowCount % 2 == 0 ? 'even' : 'odd').trim(" ".$rowClass));
+		foreach($values as $k => $v) {
+			$this->rows .= sprintf("\t\t<td class='%s'%s>%s</td>\n", $classes[$k], ($colspan ? " colspan='$colspan'" : ""), $v);
+		}
+		$this->rows .= "\t</tr>\n";
+
 		return $this;
+	}
+
+	public function buildHtml () {
+		$this->setHtml($this->start.$this->header.$this->footer.$this->rows.$this->end);
+		return $this->html;
 	}
 
 	public function addHtml ($html) {
@@ -53,11 +70,11 @@ class Table {
 			$classes = $this->rowClasses;
 		}
 
-		$this->html .= "\t<thead>\n";
+		$this->header .= "\t<thead><tr>\n";
 		foreach($values as $k => $v) {
-			$this->html .= sprintf("\t\t<th class='%s'>%s</th>\n", $classes[$k], $v);
+			$this->header .= sprintf("\t\t<th class='%s'>%s</th>\n", $classes[$k], $v);
 		}
-		$this->html .= "\t</thead>\n";
+		$this->header .= "\t</tr></thead>\n";
 		return $this;
 	}
 
@@ -66,21 +83,20 @@ class Table {
 			$classes = $this->rowClasses;
 		}
 
-		$this->html .= "\t<tfoot>\n";
+		$this->footer .= "\t<tfoot><tr>\n";
 		foreach($values as $k => $v) {
-			$this->html .= sprintf("\t\t<td class='%s'>%s</td>\n", $classes[$k], $v);
+			$this->footer .= sprintf("\t\t<td class='%s'>%s</td>\n", $classes[$k], $v);
 		}
-		$this->html .= "\t</tfoot>\n";
+		$this->footer .= "\t</tr></tfoot>\n";
 		return $this;
 	}
 
 	public function addCaption($cap) {
-		$this->html .= "\t<caption>$cap</caption>\n";
+		$this->caption .= "\t<caption>$cap</caption>\n";
 		return $this;
 	}
 
 	public function html () {
-		$this->html .= "</table>\n";
-		return $this->html;
+		return $this->buildHtml();
 	}
 }
